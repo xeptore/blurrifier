d3a5e6ba90b1bee9ed63575f15daaa7658853e8e
mehboodian <xeptore@gmail.com>
2021-03-02 14:13:38 +0330

    ⚙️ move configuration parameters to config.h

---
+++ config.h
@@ -0,0 +1,3 @@
+const uint8_t KERNEL_RADIUS = 5u; /* results in a (2*KERNEL_RADIUS+1)*(2*KERNEL_RADIUS+1) kernel window */
+const char *INPUT_FILENAME = "image.jpg";
+const char *OUTPUT_FILENAME = "image-blurred.jpg";
