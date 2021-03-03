719c426c6865a2b05c76dec12c0991d5a3f51b1f
mehboodian <xeptore@gmail.com>
2021-03-03 09:43:18 +0330

    📒 add configuration parameter comments

--- config.h
+++ config.h
@@ -1,3 +1,6 @@
-const uint8_t KERNEL_RADIUS = 5u; /* results in a (2*KERNEL_RADIUS+1)*(2*KERNEL_RADIUS+1) kernel window */
+// Results in a ((2 * KERNEL_RADIUS + 1) * (2 * KERNEL_RADIUS + 1)) kernel window
+const uint8_t KERNEL_RADIUS = 5u;
+
 const char *INPUT_FILENAME = "image.jpg";
-const char *OUTPUT_FILENAME = "image-blurred.jpg";
+
+const char *OUTPUT_FILENAME = "image-blurred-5.jpg";
