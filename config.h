deddfeabb54bfa27755623bb7cd455f32a462e93
mehboodian <xeptore@gmail.com>
2021-04-23 18:47:59 +0430

    chore: change default configuration setting values

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 // Results in a ((2 * KERNEL_RADIUS + 1) * (2 * KERNEL_RADIUS + 1)) kernel window
 const uint8_t KERNEL_RADIUS = 20U;
 
-const unsigned short int NUM_THREADS = 1U;
+const unsigned short int NUM_THREADS = 4U;
 
-const char *INPUT_IMAGE_FILENAME = "images/pcwallpapersgirls3k815.jpg";
+const char *INPUT_IMAGE_FILENAME = "images/01.jpg";
 
 const char *OUTPUT_IMAGE_FILENAME = "images/01-blurred-gaussian-20.jpg";
