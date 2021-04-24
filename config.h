6621cce24daad97dc764036291d8a97a204211ab
mehboodian <xeptore@gmail.com>
2021-04-24 08:32:03 +0430

    refactor: make configuration parameters static

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 // Results in a ((2 * KERNEL_RADIUS + 1) * (2 * KERNEL_RADIUS + 1)) kernel window
-const uint8_t KERNEL_RADIUS = 20U;
+static const uint8_t KERNEL_RADIUS = 20U;
 
-const unsigned short int NUM_THREADS = 4U;
+static const unsigned short int NUM_THREADS = 4U;
 
-const char *INPUT_IMAGE_FILENAME = "images/01.jpg";
+static const char *INPUT_IMAGE_FILENAME = "images/01.jpg";
 
-const char *OUTPUT_IMAGE_FILENAME = "images/01-blurred-gaussian-20.jpg";
+static const char *OUTPUT_IMAGE_FILENAME = "images/01-blurred-gaussian-20.jpg";
