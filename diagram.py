2a8334c7eeb8a929beb2ea21bac646e37c1c57fc
mehboodian <29199390+xeptore@users.noreply.github.com>
2021-05-17 10:49:22 +0430

    docs: add openmp results benchmark diagram drawer

--- diagram.py
+++ diagram.py
@@ -5,12 +5,27 @@ df = pd.read_csv("./results.csv")
 
 df['time'] = df['time'] * 1e-9
 
-fig = px.line(
+pthread_fig = px.line(
     df,
-    title="Kernel Radius Per Number of Workers Processing Times",
+    title="Kernel Radius Per Number of Workers Processing Times (pthread)",
     x="radius",
     y="time",
     color="workers",
     labels={ "time": "Duration (seconds)", "radius": "Kernel Radius" }
 )
-fig.show()
+
+df = pd.read_csv("./openmp.csv")
+
+df['time'] = df['time'] * 1e-9
+
+openmp_fig = px.line(
+    df,
+    title="Kernel Radius Per Number of Workers Processing Times (OpenMP)",
+    x="radius",
+    y="time",
+    color="workers",
+    labels={ "time": "Duration (seconds)", "radius": "Kernel Radius" }
+)
+
+openmp_fig.show()
+pthread_fig.show()
